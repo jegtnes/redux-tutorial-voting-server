f501618c6daf359d4e11d8922eb28fc790532c80
jegtnes <alex.jegtnes@cxpartners.co.uk>
2015-12-21 15:41:13 +0100

    Ensuring setEntries converts the value passed into an immutable list

--- src/core.js
+++ src/core.js
@@ -1,5 +1,11 @@
+import {List} from 'immutable';
+
 export function setEntries(state, entries) {
 
   // sets an entries key in the state Map, and sets value of that to passed entries
-  return state.set('entries', entries);
+  // Returning an explicit List instead of whatever was passed to this function
+  // ensures that anything iterable passed to it gets converted into an Immutable List
+  // iterable objcts have a .next() method.
+  // examples of native iterable objects are arrays, maps, strings, and sets.
+  return state.set('entries', List(entries));
 }
