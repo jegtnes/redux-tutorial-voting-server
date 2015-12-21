85e3aba2f56de59123acb5d23d2cdc3ede709080
jegtnes <alex.jegtnes@cxpartners.co.uk>
2015-12-21 15:44:44 +0100

    Better words

--- src/core.js
+++ src/core.js
@@ -1,11 +1,12 @@
 import {List} from 'immutable';
 
 export function setEntries(state, entries) {
-
   // sets an entries key in the state Map, and sets value of that to passed entries
-  // Returning an explicit List instead of whatever was passed to this function
-  // ensures that anything iterable passed to it gets converted into an Immutable List
-  // iterable objcts have a .next() method.
-  // examples of native iterable objects are arrays, maps, strings, and sets.
+
+  // converts the data type of the entries argument to an Immutable List
+  // instead of assuming it is an Immutable List already. This brings benefits.
+  // We can pass any iterable object (arrays, maps, strings, and sets) now.
+  // Any object is iterable if it implements the .next() method correctly.
+  // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Iteration_protocols
   return state.set('entries', List(entries));
 }
