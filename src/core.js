9a703d11c101e6188668f38932b5581fc55b75cd
jegtnes <alex.jegtnes@cxpartners.co.uk>
2015-12-22 20:13:09 +0100

    Added failing test for putting films to a vote

--- src/core.js
+++ src/core.js
@@ -1,7 +1,7 @@
 import {List} from 'immutable';
 
+// sets an entries key in the state Map, and sets value of that to passed entries
 export function setEntries(state, entries) {
-  // sets an entries key in the state Map, and sets value of that to passed entries
 
   // converts the data type of the entries argument to an Immutable List
   // instead of assuming it is an Immutable List already. This brings benefits.
@@ -9,4 +9,8 @@ export function setEntries(state, entries) {
   // Any object is iterable if it implements the .next() method correctly.
   // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Iteration_protocols
   return state.set('entries', List(entries));
+}
+
+export function next(state) {
+  return state;
 }
--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,7 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('application logic', () => {
   describe ('setEntries', () => {
@@ -22,6 +22,23 @@ describe('application logic', () => {
       const nextState = setEntries(state, entries);
       expect(nextState).to.equal(Map({
         entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    });
+  });
+
+  describe('next', () => {
+    it('takes the next two entries and puts them to a vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      });
+
+      const nextState = next(state);
+
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
       }));
     });
   });
