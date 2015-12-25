aec237fabd3caf0b09143f932bd147a8395443e6
jegtnes <alex.jegtnes@cxpartners.co.uk>
2015-12-26 00:24:09 +0100

    Attempting to explain reduction

--- src/reducer.js
+++ src/reducer.js
@@ -7,9 +7,13 @@ import {setEntries, next, vote, INITIAL_STATE} from './core';
 // Every action has a type, but may have additional attributes.
 // Redux also needs a way to turn an action into a function call,
 // but for that to work, you need a middle-man. This middle-man
-// is a reducer.
-// If the action is unknown, we return the current state.
-// No harm done.
+// is a reducer. It's called a reducer because it's compatible
+// with native JS array reduction:
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+// Array reduction is taking a collection of variables aiming to
+// do stuff with them and ending up with a single value. Hence, reduction.
+// In a Redux sense, I think this means that we can have a bunch
+// of actions, and and we eventually end up at a final state.
 
 export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
