c051615500cff6905941e4126e4e654a767def50
jegtnes <alex.jegtnes@cxpartners.co.uk>
2015-12-27 23:00:55 +0100

    More socket setups

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ import makeStore from './src/store';
 import startServer from './src/socketServer';
 
 export const store = makeStore();
-startServer();
+
+// Subscribing to a Redux store:
+startServer(store);
+
+// On starting the application, dispatch a set entries action to Redux
+// and activates the next state (voting).
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')
+});
+store.dispatch({type: 'NEXT'});
--- src/socketServer.js
+++ src/socketServer.js
@@ -1,5 +1,20 @@
 import Server from 'socket.io';
 
-export default function StartServer() {
+export default function StartServer(store) {
   const io = new Server().attach(8090);
+
+  // whenever the store state changes, emit this change to open web sockets:
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  );
+
+  io.on('connection', (socket) => {
+    // On receiving an initial socket connection, send the app state to it
+    // for larger applications or anywhere there are security risks involved
+    // this will obvs not be ideal. but for this tutorial, this will do
+    socket.emit('state', store.getState().toJS());
+
+    // whenever an action happens on the client, forward it to the Redux store
+    socket.on('action', store.dispatch.bind(store));
+  });
 }
